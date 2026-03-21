5fb18e724e41b618bb2fa4f5e8d727f14d04bc8f
tebogo60 <tjjmalebana@gmail.com>
2026-03-22 00:12:53 +0200

    feat(repo): add verification token lookup query

--- src/main/java/com/cassinocards/cassino_api/repository/user/UnverifiedUserRepository.java
+++ src/main/java/com/cassinocards/cassino_api/repository/user/UnverifiedUserRepository.java
@@ -5,11 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+import java.util.UUID;
 
 @Repository
 public interface UnverifiedUserRepository extends JpaRepository<UnverifiedUser, Long> {
 
     Optional<UnverifiedUser> findUnverifiedUserByEmail(String email);
+    Optional<UnverifiedUser> findUnverifiedUserByVerificationToken(UUID token);
+
     Boolean existsAllByEmail(String email);
-//    Boolean validToken(String email, String token);
+    Boolean existsByVerificationToken(UUID token);
 }
