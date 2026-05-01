43af58969680b7b4df4aa3e1cd76a93208ed0379
tebogo60 <tjjmalebana@gmail.com>
2026-05-01 14:04:17 +0200

    feat: add dto for reset password

--- src/main/java/com/cassinocards/cassino_api/repository/user/PasswordResetTokenRepository.java
+++ src/main/java/com/cassinocards/cassino_api/repository/user/PasswordResetTokenRepository.java
@@ -3,5 +3,9 @@ package com.cassinocards.cassino_api.repository.user;
 import com.cassinocards.cassino_api.model.user.PasswordResetToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+import java.util.UUID;
+
 public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, Long> {
+    Optional<PasswordResetToken> findByToken(UUID token);
 }
