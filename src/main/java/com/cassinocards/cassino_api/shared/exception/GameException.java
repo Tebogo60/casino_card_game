cd617b8a8aab4aac04b55e1b10a299bc2c77c422
tebogo60 <tjjmalebana@gmail.com>
2026-05-13 00:16:22 +0200

    feat: add core GameException

---
+++ src/main/java/com/cassinocards/cassino_api/shared/exception/GameException.java
@@ -0,0 +1,7 @@
+package com.cassinocards.cassino_api.shared.exception;
+
+public class GameException extends RuntimeException {
+    public GameException(String message) {
+        super(message);
+    }
+}
